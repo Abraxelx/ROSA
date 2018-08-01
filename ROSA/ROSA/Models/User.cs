c3244386f09d0cbd028c48b3657175720ef32e66
sinan kogce <kogceweb@gmail.com>
2018-08-01 21:48:00 +0300

    web service data modeller olusturuldu
    web service data modeller olusturuldu

---
+++ ROSA/ROSA/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ROSA.Models
+{
+    public class User
+    {
+        private string Username { get; set; }
+
+        private string Password { get; set; }
+
+        private string UserLoginType { get; set; }
+
+
+    }
+}

55dea0685fd2a241479929e85cd2f45eb6c1714d
sinan kogce <kogceweb@gmail.com>
2018-08-03 19:44:22 +0300

    comment service upgraded

--- ROSA/ROSA/Services/ICommentRestService.cs
+++ ROSA/ROSA/Services/ICommentRestService.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using ROSA.Models;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace ROSA.Services
 {
     interface ICommentRestService
     {
+         Task<List<Comment>> RefreshDataAsync();
+
+        Task SaveCommentAsync(Comment item, bool isNewItem);
+
+        Task DeleteCommentAsync(string id);
     }
 }
